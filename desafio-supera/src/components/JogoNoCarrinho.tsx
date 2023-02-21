79170049833f02d8c92b16a3c6b4da9979caed09
Eduardo Pontes <eduardo.pontes2801@gmail.com>
2023-02-20 21:41:48 -0300

    feat/cartComponentAndLogic

---
+++ desafio-supera/src/components/JogoNoCarrinho.tsx
@@ -0,0 +1,40 @@
+import { useCarrinho } from "../context/carrinhoContext"
+// Importando JSON de produtos
+import storeGames from "../data/products.json"
+// Imports do Bootstrap
+import { Button, Stack } from 'react-bootstrap';
+import { formataVM } from "../utils/formataVM";
+
+type JogoNoCarrinhoProps = {
+    id: number;
+    quantidade: number;
+}
+
+
+export function JogoNoCarrinho({ id, quantidade }: JogoNoCarrinhoProps) {
+    const { removeGamesQuantity } = useCarrinho();
+    const jogo = storeGames.find(jogo => jogo.id === id);
+    
+    // Se o jogo não existir, retorna null
+    if(jogo== null) return null;
+
+    return (
+        // Render dos jogos selecionados no carrinho de compras
+        <Stack direction="horizontal" gap={2}>
+            <img src={jogo?.image} alt={jogo?.name} width="70" height="70" style={{ objectFit: 'cover' }}/>
+            <div className="me-auto">
+                {jogo?.name}{" "}{quantidade > 1 && (<span className="text-muted">{quantidade}x</span>)}
+            </div>
+            <div className="text-muted">
+                {formataVM(jogo.price)}
+            </div>
+            <div>
+                {formataVM(jogo.price * quantidade)}
+            </div>
+
+            <Button variant="outline-danger" size="sm" onClick={() => removeGamesQuantity(jogo.id)}>X</Button>
+
+
+        </Stack>
+    );
+}
--- desafio-supera/src/context/carrinhoContext.tsx
+++ desafio-supera/src/context/carrinhoContext.tsx
@@ -1,4 +1,6 @@
 import { createContext, useContext, ReactNode, useState  } from "react";
+// Importando componente carrinho de compras modal
+import { ModalCarrinho } from "../components/ModalCarrinho";
 
 // Tipagem do provider do carrinho de compras (children é o que vai ser renderizado dentro do provider)
 type CarrinhoProviderProps = {
@@ -6,19 +8,31 @@ type CarrinhoProviderProps = {
     children: ReactNode;
 }
 
-// Tipagem do dados que o carrinho de compras vai receber
-type CarrinhoContextData = {
-    getGamesQuantity: (id: number) => number;
-    addGamesQuantity: (id: number) => void;
-    removeGamesQuantity: (id: number) => void;
-    removerDoCarrinho: (id: number) => void;
-}
-
-// Tipagem do carrinho de compras (id do jogo e quantidade)
+// Tipagem do jogo no carrinho de compras (id do jogo e quantidade)
 type CarrinhoJogo = {
     id: number;
     quantidade: number;
 }
+
+// Tipagem de funções que serão passadas para o contexto do carrinho de compras
+type CarrinhoContextData = {
+    /*
+        Funções para pegar a quantidade de um jogo no carrinho de compras, adicionar a quantidade de um jogo no carrinho de compras, remover a quantidade de um jogo no carrinho de compras e remover um jogo do carrinho de compras    
+    */
+    getGamesQuantity: (id: number) => number;
+    addGamesQuantity: (id: number) => void;
+    removeGamesQuantity: (id: number) => void;
+    removerDoCarrinho: (id: number) => void;
+
+    /*
+        Funções referentes a modal do carrinho (abrir e fechar)
+    */
+    abrirModalCarrinho: () => void;
+    fecharModalCarrinho: () => void;
+    quantidadeTotalNoCarrinho: number;
+    carrinhoJogos: CarrinhoJogo[];
+}
+
 
 // Criando contexto do carrinho de compras
 const CarrinhoContext = createContext({} as CarrinhoContextData);
@@ -30,24 +44,37 @@ export function useCarrinho() {
 
 // Provider do carrinho de compras (passa todos os dados do carrinho de compras e renderiza a view do mesmo)
 export function CarrinhoProvider({ children }: CarrinhoProviderProps) {
+
     // Usando o hook useState para ter aonde armazenar os dados do carrinho de compras
-    const [carrinhoJogo, setCarrinhoJogo] = useState<CarrinhoJogo[]>([]);
+    const [carrinhoJogos, setCarrinhoJogos] = useState<CarrinhoJogo[]>([]);
+
+    // Usando o hook useState para abrir e fechar a modal do carrinho de compras
+    const [modalCarrinhoAberto, setModalCarrinhoAberto] = useState(false);
+    
+    // Função para abrir a modal do carrinho de compras
+    const abrirModalCarrinho = () => setModalCarrinhoAberto(true);
+
+    // Função para fechar a modal do carrinho de compras
+    const fecharModalCarrinho = () => setModalCarrinhoAberto(false);
+
+    // Função para pegar a quantidade total de itens no carrinho de compras
+    const quantidadeTotalNoCarrinho = carrinhoJogos.reduce((quantidade, jogo) => jogo.quantidade + quantidade, 0);
 
     // Função para pegar a quantidade de um jogo no carrinho de compras
     function getGamesQuantity(id: number) {
-        return carrinhoJogo.find(jogo => jogo.id === id)?.quantidade || 0;
+        return carrinhoJogos.find(jogo => jogo.id === id)?.quantidade || 0;
     }
 
     // Função para adicionar a quatidade de um jogo no carrinho de compras
     function addGamesQuantity(id: number) {
-        setCarrinhoJogo(carrinhoJogo => {
+        setCarrinhoJogos(carrinhoJogos => {
             // Verificando se o jogo já está no carrinho de compras
-            if (carrinhoJogo.find(jogo => jogo.id === id) == null) {
+            if (carrinhoJogos.find(jogo => jogo.id === id) == null) {
                 // Se não estiver, adiciona o jogo no carrinho de compras
-                return [...carrinhoJogo, { id, quantidade: 1 }];
+                return [...carrinhoJogos, { id, quantidade: 1 }];
             } else {
                 // Se estiver um jogo com o mesmo id, incrementa a quantidade do jogo no carrinho de compras
-                return carrinhoJogo.map(jogo => {
+                return carrinhoJogos.map(jogo => {
                     if (jogo.id === id) {
                         return { ...jogo, quantidade: jogo.quantidade + 1 };
                     } else {
@@ -61,13 +88,13 @@ export function CarrinhoProvider({ children }: CarrinhoProviderProps) {
 
     // Função para remover a quantidade de um jogo do carrinho de compras
     function removeGamesQuantity(id: number) {
-        setCarrinhoJogo(carrinhoJogo => {
+        setCarrinhoJogos(carrinhoJogos => {
             // Verificando se a quantidade do jogo é 1 para remover o jogo com esse id do carrinho de compras
-            if(carrinhoJogo.find(jogo => jogo.id === id)?.quantidade === 1) {
-                return carrinhoJogo.filter(jogo => jogo.id !== id);
+            if(carrinhoJogos.find(jogo => jogo.id === id)?.quantidade === 1) {
+                return carrinhoJogos.filter(jogo => jogo.id !== id);
             } else {
                 // Se a quantidade do jogo for maior que 1, decrementa a quantidade do jogo no carrinho de compras
-                return carrinhoJogo.map(jogo => {
+                return carrinhoJogos.map(jogo => {
                     if (jogo.id === id) {
                         return { ...jogo, quantidade: jogo.quantidade - 1 };
                     } else {
@@ -80,15 +107,28 @@ export function CarrinhoProvider({ children }: CarrinhoProviderProps) {
 
     // Função para remover um jogo do carrinho de compras
     function removerDoCarrinho(id: number) {
-        setCarrinhoJogo(carrinhoJogo => {
+        setCarrinhoJogos(carrinhoJogos => {
             // Filtrando o carrinho de compras para remover o jogo com o id passado
-            return carrinhoJogo.filter(jogo => jogo.id !== id);
+            return carrinhoJogos.filter(jogo => jogo.id !== id);
         });
     }
 
     return (
-        <CarrinhoContext.Provider value={{ getGamesQuantity, addGamesQuantity, removeGamesQuantity, removerDoCarrinho }}>
+        <CarrinhoContext.Provider 
+            value={{
+                getGamesQuantity,
+                addGamesQuantity,
+                removeGamesQuantity,
+                removerDoCarrinho,
+                carrinhoJogos,
+                quantidadeTotalNoCarrinho,
+                abrirModalCarrinho,
+                fecharModalCarrinho
+            }}
+        >
             {children}
+            {/* Render do modal do carrinho de compras */}
+            <ModalCarrinho modalCarrinhoAberto={modalCarrinhoAberto} />
         </CarrinhoContext.Provider>
     );
 }
